3c3864d7282510863bbe848d96226568a52ebf7b
ozanu <ozanu70@gmail.com>
2020-11-23 23:37:31 +0300

    weather message changed

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'If you want to learn the weather in your city, please enter city name.',
         title: 'Help',
         name: 'Andrew Mead'
     })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, 
+                body.current.weather_descriptions[0] + 
+                " It is currently " + body.current.temperature + " degress out.  It fells like " + 
+                body.current.feelslike + " degrees out. The humadity is " + body.current.humidity + "%." 
+            )
         }
     })
 }
